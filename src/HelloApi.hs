71cca13f9d212022736244291eb201d4866f513f
Brian Henning <henninb@msn.com>
2020-10-15 07:03:03 -0500

    updates

--- src/HelloApi.hs
+++ src/HelloApi.hs
@@ -12,6 +12,7 @@ import           GHC.Generics
 import           Servant.API
 
 -- http://localhost:8080/user?name=brian&age=25
+-- http://localhost:8080?name=brian&age=25
 
 data User = User
     { name :: Text
@@ -21,7 +22,8 @@ instance FromJSON User
 instance ToJSON User
 
 type HelloAPI  = Get '[PlainText] Text
-            :<|> "user" :> Capture "name" Text :> Capture "age" Int :> Get '[JSON] User
+--            :<|> "user" :> Capture "name" Text :> Capture "age" Int :> Get '[JSON] User
+            :<|> Capture "name" Text :> Capture "age" Int :> Get '[JSON] User
 
 helloApi :: Proxy HelloAPI
 helloApi = Proxy
